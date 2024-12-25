2309797c05fc4f6d97fe93b52a3236ed44a2df08
oleksii-yer <oleksiiyer@gmail.com>
2024-12-25 16:56:31 +0100

    Updated UI

--- frontend/app.py
+++ frontend/app.py
@@ -17,18 +17,51 @@ with col1:
     img = Image.open("imgs/team16_6_Logo.png")  # Use the relative path or absolute path
     st.image(img, use_container_width=True)
 
+
 # Title column
 with col2:
     st.title("Group 4. Comment classification using BERT")
 
+
 # Option menu
-selected = option_menu(menu_title=None, options=["Home", "Classify"],
-                       menu_icon="cast", default_index=0, icons=['house', 'play'],
+selected = option_menu(menu_title=None, options=["Home", "Team", "Classify"],
+                       menu_icon="cast", default_index=0, icons=['house', 'people', 'play'],
                        orientation="horizontal")
+
 
 # Home page
 if selected == 'Home':
-    st.write('To be added...')
+    st.title('Welcome!')
+    st.write('Use this app to analyse texts for toxicity.')
+    to_classify = st.button('Go to Classify')
+    if to_classify:
+        selected = 'Classify'
+
+
+# Team page
+if selected == 'Team':
+    st.title('Our Team')
+
+    st.markdown("---")
+
+    team = [
+        {"name": "Serhii Trush", "role": "Team Lead"},
+        {"name": "Oleksandr Kovalenko", "role": "SCRUM Master"},
+        {"name": "Aliona Mishchenko", "role": "Data Scientist"},
+        {"name": "Ivan Shkvir", "role": "Backend Developer"},
+        {"name": "Oleksii Yeromenko", "role": "Frontend Developer"},
+        {"name": "Polina Mamchur", "role": "Creative Director"}
+    ]
+
+    # Display team members in columns
+    for i in range(0, len(team), 2):  # Display 2 members per row
+        cols = st.columns(2)
+        for col, member in zip(cols, team[i:i+2]):
+            with col:
+                # Display member name and role
+                st.markdown(f"### {member['name']}")
+                st.markdown(f"*{member['role']}*")
+
 
 # Classification page
 elif selected == 'Classify':
@@ -41,6 +74,12 @@ elif selected == 'Classify':
 
     # User's comment input
     user_comment = st.text_area('Enter your comment here')
+
+    # User's text file upload
+    uploaded_file = st.file_uploader("Upload your text file", type=["txt"])
+
+    if uploaded_file is not None:
+        user_comment = uploaded_file.read().decode("utf-8")
 
     # Toxicity probabilities checkbox
     detailed_classification = st.checkbox('Display toxicity probabilities')
